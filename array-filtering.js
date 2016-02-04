9901fa0089b5f5c678518bedc06640e608599f77
simonrossi <simonrossi@seattleacademy.org>
2016-02-04 09:02:10 -0800

    complete array-filtering.js challenge

--- array-filtering.js
+++ array-filtering.js
@@ -23,3 +23,10 @@
 
      javascripting verify array-filtering.js
 */
+var numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function evenNumbers (number) {
+return number % 2===0;
+
+}
+filtered=numbers.filter(evenNumbers);
+console.log(filtered);
