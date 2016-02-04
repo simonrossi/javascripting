64ca1d4801a1dd71ddc8dd728b85f015c311ab44
simonrossi <simonrossi@seattleacademy.org>
2016-02-04 08:27:26 -0800

    complete introduction.js challenge

--- introduction.js
+++ introduction.js
@@ -1,2 +1,3 @@
 //Type below:  console.log('hello');
 //From the command line, enter:  javascripting verify introduction
+console.log('hello');
--- strings.js
+++ strings.js
@@ -9,3 +9,6 @@
 */
 
 
+var someString = 'this is a string';
+console.log(someString);
+
